1afb07d2d0955cd19a33bd830ede6133d149fa5a
Philip Lisak <phil.lisak@gmail.com>
2020-07-09 20:22:11 -0700

    mouseenter and mouseleave implemented on fun bus img

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+const funBus = document.querySelector('.intro img');
+funBus.addEventListener('mouseenter', () => {
+    funBus.src = "https://funbuses.com/oak/files/bus/buscropped-onrainbow.jpg"
+    funBus.style.transition = "0.3s"
+})
+funBus.addEventListener('mouseleave', () => {
+    funBus.src = 'img/fun-bus.jpg'
+})
