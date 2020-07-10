5f3f20dae5b2a53e1accd11dfc9999d9f97ab6b7
Phil Lisak <phil.lisak@gmail.com>
2020-07-10 16:26:08 -0700

    Got the mouseover and mouseout to work. Need to figure out why my img is broken on mouseleave

--- js/index.js
+++ js/index.js
@@ -3,8 +3,20 @@
 const funBus = document.querySelector('.intro img');
 funBus.addEventListener('mouseenter', () => {
     funBus.src = "https://funbuses.com/oak/files/bus/buscropped-onrainbow.jpg"
-    funBus.style.transition = "0.3s"
+    funBus.style.transition = "all 0.3s"
 })
-funBus.addEventListener('mouseleave', () => {
-    funBus.src = 'img/fun-bus.jpg'
+funBus.addEventListener('mouseleave', (event) => {
+    event.target.src = "img/fun-bus.jpg";
+})
+
+const navList = document.querySelectorAll('nav a')
+navList.forEach(item => {
+    item.addEventListener('mouseover', event => {
+        event.target.style.color = 'red'
+    })
+    navList.forEach(item => {
+        item.addEventListener('mouseout', event => {
+            event.target.style.color = ""
+        })
+    })
 })
