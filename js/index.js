e2cd196fc10e5aa274bea36859d23475a54322ff
Phil Lisak <phil.lisak@gmail.com>
2020-07-10 18:50:12 -0700

    Switching from desktop to laptop

--- js/index.js
+++ js/index.js
@@ -11,10 +11,13 @@ const navList = document.querySelectorAll('nav a')
 navList.forEach(item => {
     item.addEventListener('mouseover', event => {
         event.target.style.color = 'red'
+        event.target.style.transform = 'scale(1.2)'
+        event.target.style.transition = 'all 0.3s'
     })
     navList.forEach(item => {
         item.addEventListener('mouseout', event => {
             event.target.style.color = ""
+            event.target.style.transform = 'scale(1.0)'
         })
     })
 })
@@ -28,5 +31,13 @@ const headLines = document.querySelectorAll('h2')
 headLines.forEach(item => {
     item.addEventListener('dblclick', event => {
         item.textContent = 'Only once is necessary, thanks.';
+    })
+})
+
+const imgContent = document.querySelectorAll('.img-content img')
+
+imgContent.forEach(item => {
+    item.addEventListener('drag', (event) => {
+        imgContent.alert("I do not consent to this!");
     })
 })
