b48170753a5a696ec9d1960c1ea9ae5eb1570c9c
Phil Lisak <phil.lisak@gmail.com>
2020-07-10 18:34:43 -0700

    Figured out the click and dblclick

--- js/index.js
+++ js/index.js
@@ -1,12 +1,10 @@
 // Your code goes here
 
 const funBus = document.querySelector('.intro img');
-funBus.addEventListener('mouseenter', () => {
+funBus.addEventListener('mouseenter', (event) => {
     funBus.src = "https://funbuses.com/oak/files/bus/buscropped-onrainbow.jpg"
     funBus.style.transition = "all 0.3s"
-})
-funBus.addEventListener('mouseleave', (event) => {
-    event.target.src = "img/fun-bus.jpg";
+    event.stopPropagation();
 })
 
 const navList = document.querySelectorAll('nav a')
@@ -18,5 +16,17 @@ navList.forEach(item => {
         item.addEventListener('mouseout', event => {
             event.target.style.color = ""
         })
+    })
+})
+
+const logoHead = document.querySelector('.logo-heading')
+logoHead.addEventListener('click', () => {
+    logoHead.style.transform = 'scale(1.5)'
+})
+
+const headLines = document.querySelectorAll('h2')
+headLines.forEach(item => {
+    item.addEventListener('dblclick', event => {
+        item.textContent = 'Only once is necessary, thanks.';
     })
 })
