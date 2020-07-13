9fbd761589fbe92e772b056b10bc84031fa0d1c0
Phil Lisak <phil.lisak@gmail.com>
2020-07-12 18:07:38 -0700

    Setting up stretch

---
+++ stretch-assignment/index.js
@@ -0,0 +1,16 @@
+// Variables
+
+blocks = document.querySelector('.blocks');
+redBlock = document.querySelector('.block--red');
+blueBlock = document.querySelector('.block--blue');
+greenBlock = document.querySelector('.block--green');
+pinkBlock = document.querySelector('.block--pink');
+grayBlock = document.querySelector('.block--gray');
+
+// Rules/Functions
+
+Array.from(blocks);
+console.log("test", blocks)
+
+// ============================================= MAIN ===========================================
+
